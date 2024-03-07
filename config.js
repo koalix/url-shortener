b8bec39ee5fdf6d00c550d9b21d4facab4741195
Fernando Coutinho <dev@koalas-inn.com>
2024-03-07 01:30:10 -0300

    Commit inicial: sistema base de encurtamento de URL

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+require('dotenv').config();
+
+module.exports = {
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  },
+  baseUrl: process.env.BASE_URL
+};
